679ae805475ae79e614fe493ba6f7a0f9e2b9f2b
Michael Jancar <mikejancar@outlook.com>
2020-03-22 10:41:13 -0400

    Moved box score data retrieval into data service

--- src/box-scores/box-scores.service.ts
+++ src/box-scores/box-scores.service.ts
@@ -4,18 +4,15 @@ import fs from 'fs';
 import fetch from 'node-fetch';
 
 import { Constants } from '../app.constants';
+import { DataService } from '../data/data.service';
 import { FormattingService } from '../formatting/formatting.service';
-import { AdvancedTeamStatsColumns } from '../models/advanced-team-stats-columns.enum';
-import { AdvancedTeamsStatsResponse } from '../models/advanced-team-stats-response';
 import { BoxScore } from '../models/box-score';
-import { BoxScoreColumns } from '../models/box-score-columns.enum';
 import { BoxScoreResponse } from '../models/box-score-response';
 import { BoxScoreSummary } from '../models/box-score-summary';
-import { BoxScoreTeam } from '../models/box-score-team';
 
 @Injectable()
 export class BoxScoresService {
-  constructor(private formattingService: FormattingService) { }
+  constructor(private dataService: DataService, private formattingService: FormattingService) { }
 
   getBoxScoresOn(datePlayed: string): any {
     const dateFormatted = this.formattingService.formatDateForStatsCall(datePlayed);
@@ -73,7 +70,7 @@ export class BoxScoresService {
 
     while (firstDate <= lastDate) {
       const dateFormatted = format(firstDate, 'yyyyMMdd');
-      boxScoreSummary.boxScores.push(...this.getEnhancedBoxScores(dateFormatted));
+      boxScoreSummary.boxScores.push(...this.dataService.getEnhancedBoxScores(dateFormatted));
       firstDate = addDays(firstDate, 1);
     }
 
@@ -88,84 +85,6 @@ export class BoxScoresService {
       moreDefensivelyEfficient: this.formattingService.roundToNthDigit(boxScoreSummary.boxScores.filter((boxScore: BoxScore) => boxScore.winningCharacteristics.moreDefensivelyEfficient).length / totalBoxScores, 3),
       hadHigherWinningPercentage: this.formattingService.roundToNthDigit(boxScoreSummary.boxScores.filter((boxScore: BoxScore) => boxScore.winningCharacteristics.hadHigherWinningPercentage).length / totalBoxScores, 3),
       averagePointGap: this.formattingService.roundToNthDigit(boxScoreSummary.boxScores.map(box => box.winningCharacteristics.pointGap).reduce((accum, next) => accum + next) / totalBoxScores, 2)
-    };
-  }
-
-  getEnhancedBoxScores(datePlayed: string): BoxScore[] {
-    const dateFormatted = this.formattingService.formatDateForFileName(datePlayed);
-    const boxScoreFilePath = `${Constants.dataDirectory}\\box-scores-${dateFormatted}.json`
-    const boxScoreData: BoxScoreResponse = JSON.parse(fs.readFileSync(boxScoreFilePath).toString());
-
-    const boxScores: BoxScore[] = [];
-
-    for (let i = 0; i < boxScoreData.resultSets[0].rowSet.length; i += 2) {
-      const rowOne: any[] = boxScoreData.resultSets[0].rowSet[i];
-      const rowTwo: any[] = boxScoreData.resultSets[0].rowSet[i + 1];
-
-      const teamOne: BoxScoreTeam = this.createTeamFromBoxScore(rowOne);
-      const teamTwo: BoxScoreTeam = this.createTeamFromBoxScore(rowTwo);
-
-      const boxScore: BoxScore = {
-        homeTeam: rowOne[BoxScoreColumns.MATCHUP].includes('@') ? teamTwo : teamOne,
-        awayTeam: rowOne[BoxScoreColumns.MATCHUP].includes('@') ? teamOne : teamTwo,
-        datePlayed: rowOne[BoxScoreColumns.GAME_DATE]
-      };
-      this.enhanceBoxScore(boxScore);
-      this.determineWinningCharacteristics(boxScore);
-      boxScores.push(boxScore);
-    }
-    return boxScores;
-  }
-
-  createTeamFromBoxScore(row: any[]): BoxScoreTeam {
-    return {
-      teamId: row[BoxScoreColumns.TEAM_ID],
-      teamName: row[BoxScoreColumns.TEAM_NAME],
-      abbreviation: row[BoxScoreColumns.TEAM_ABBREVIATION],
-      wonGame: row[BoxScoreColumns.WL] === 'W',
-      advancedStats: {
-        pointsScored: row[BoxScoreColumns.PTS]
-      }
-    };
-  }
-
-  enhanceBoxScore(boxScore: BoxScore): void {
-    const filePath = `${Constants.dataDirectory}\\advanced-team-stats-${boxScore.datePlayed}.json`;
-    const advancedStats: AdvancedTeamsStatsResponse = JSON.parse(fs.readFileSync(filePath).toString());
-    const rowSet = advancedStats.resultSets[0].rowSet;
-
-    this.loadAdvancedStats(boxScore.homeTeam, rowSet);
-    this.loadAdvancedStats(boxScore.awayTeam, rowSet);
-  }
-
-  loadAdvancedStats(team: BoxScoreTeam, rowSet: any[][]): void {
-    const teamData: any[] = rowSet.find((row: any[]) => row[AdvancedTeamStatsColumns.TEAM_ID] === team.teamId);
-    if (!teamData) {
-      const errorMessage = `Failed to find an advanced team stat record for the ${team.teamName}`;
-      console.log(errorMessage);
-      throw new Error(errorMessage);
-    }
-
-    team.advancedStats.winningPercentage = teamData[AdvancedTeamStatsColumns.W_PCT];
-    team.advancedStats.offensiveEfficiency = teamData[AdvancedTeamStatsColumns.OFF_RATING];
-    team.advancedStats.offensiveRank = teamData[AdvancedTeamStatsColumns.OFF_RATING_RANK];
-    team.advancedStats.defensiveEfficiency = teamData[AdvancedTeamStatsColumns.DEF_RATING];
-    team.advancedStats.defensiveRank = teamData[AdvancedTeamStatsColumns.DEF_RATING_RANK];
-  }
-
-  determineWinningCharacteristics(boxScore: BoxScore): void {
-    const winningTeam = boxScore.homeTeam.wonGame ? boxScore.homeTeam : boxScore.awayTeam;
-    const losingTeam = boxScore.homeTeam.wonGame ? boxScore.awayTeam : boxScore.homeTeam;
-
-    boxScore.winningCharacteristics = {
-      wasHomeTeam: boxScore.homeTeam.wonGame,
-      moreOffensivelyEfficient: winningTeam.advancedStats.offensiveEfficiency > losingTeam.advancedStats.offensiveEfficiency,
-      offensiveEfficiencyGap: this.formattingService.roundToNthDigit(winningTeam.advancedStats.offensiveEfficiency - losingTeam.advancedStats.offensiveEfficiency, 3),
-      moreDefensivelyEfficient: winningTeam.advancedStats.defensiveEfficiency < losingTeam.advancedStats.defensiveEfficiency,
-      defensiveEfficiencyGap: this.formattingService.roundToNthDigit(losingTeam.advancedStats.defensiveEfficiency - winningTeam.advancedStats.defensiveEfficiency, 3),
-      hadHigherWinningPercentage: winningTeam.advancedStats.winningPercentage > losingTeam.advancedStats.winningPercentage,
-      winningPercentageGap: this.formattingService.roundToNthDigit(winningTeam.advancedStats.winningPercentage - losingTeam.advancedStats.winningPercentage, 3),
-      pointGap: winningTeam.advancedStats.pointsScored - losingTeam.advancedStats.pointsScored
     };
   }
 }
--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -5,33 +5,34 @@ import { Constants } from '../app.constants';
 import { FormattingService } from '../formatting/formatting.service';
 import { AdvancedTeamStatsColumns } from '../models/advanced-team-stats-columns.enum';
 import { AdvancedTeamsStatsResponse } from '../models/advanced-team-stats-response';
+import { BoxScore } from '../models/box-score';
+import { BoxScoreColumns } from '../models/box-score-columns.enum';
+import { BoxScoreResponse } from '../models/box-score-response';
+import { BoxScoreTeam } from '../models/box-score-team';
 import { Team } from '../models/team';
 
 @Injectable()
 export class DataService {
   private advancedTeamData: Map<string, Team[]> = new Map();
+  private boxScoreData: Map<string, BoxScore[]> = new Map();
 
   private advancedTeamStatsPrefix = 'advanced-team-stats';
+  private boxScoresPrefix = 'box-scores';
 
   constructor(private formattingService: FormattingService) { }
 
   getAdvancedTeamStats(asOf?: string): Team[] {
     const statDate = asOf || this.getLatestFileDate(this.advancedTeamStatsPrefix);
 
-    if (!this.advancedTeamData || !this.advancedTeamData.has(statDate)) {
+    if (!this.advancedTeamData.has(statDate)) {
       this.loadTeamData(statDate);
     }
     return this.advancedTeamData.get(statDate);
   }
 
-  private getLatestFileDate(prefix: string): string {
-    const files = fs.readdirSync(Constants.dataDirectory).filter(file => file.startsWith(prefix)).sort();
-    return files[files.length - 1].replace(`${prefix}-`, '').replace('.json', '').replace(/-/g, '');
-  }
-
   private loadTeamData(asOf: string): void {
     const dateFormatted = this.formattingService.formatDateForFileName(asOf);
-    const teamStatsFilePath = `${Constants.dataDirectory}\\advanced-team-stats-${dateFormatted}.json`
+    const teamStatsFilePath = `${Constants.dataDirectory}\\${this.advancedTeamStatsPrefix}-${dateFormatted}.json`
     const teamStatsData: AdvancedTeamsStatsResponse = JSON.parse(fs.readFileSync(teamStatsFilePath).toString());
     const teamList: any[][] = teamStatsData.resultSets[0].rowSet;
 
@@ -53,5 +54,88 @@ export class DataService {
     }
 
     this.advancedTeamData.set(asOf, teams);
+  }
+
+  getEnhancedBoxScores(datePlayed: string): BoxScore[] {
+    if (!this.boxScoreData.has(datePlayed)) {
+      this.loadBoxScoreData(datePlayed);
+    }
+    return this.boxScoreData.get(datePlayed);
+  }
+
+  private loadBoxScoreData(datePlayed: string): void {
+    const dateFormatted = this.formattingService.formatDateForFileName(datePlayed);
+    const boxScoreFilePath = `${Constants.dataDirectory}\\${this.boxScoresPrefix}-${dateFormatted}.json`
+    const boxScoreData: BoxScoreResponse = JSON.parse(fs.readFileSync(boxScoreFilePath).toString());
+
+    const boxScores: BoxScore[] = [];
+
+    for (let i = 0; i < boxScoreData.resultSets[0].rowSet.length; i += 2) {
+      const rowOne: any[] = boxScoreData.resultSets[0].rowSet[i];
+      const rowTwo: any[] = boxScoreData.resultSets[0].rowSet[i + 1];
+
+      const teamOne: BoxScoreTeam = this.createTeamFromBoxScore(rowOne);
+      const teamTwo: BoxScoreTeam = this.createTeamFromBoxScore(rowTwo);
+
+      const boxScore: BoxScore = {
+        homeTeam: rowOne[BoxScoreColumns.MATCHUP].includes('@') ? teamTwo : teamOne,
+        awayTeam: rowOne[BoxScoreColumns.MATCHUP].includes('@') ? teamOne : teamTwo,
+        datePlayed: rowOne[BoxScoreColumns.GAME_DATE]
+      };
+      this.enhanceBoxScore(boxScore, datePlayed);
+      this.determineWinningCharacteristics(boxScore);
+      boxScores.push(boxScore);
+    }
+
+    this.boxScoreData.set(datePlayed, boxScores);
+  }
+
+  private createTeamFromBoxScore(row: any[]): BoxScoreTeam {
+    return {
+      teamId: row[BoxScoreColumns.TEAM_ID],
+      teamName: row[BoxScoreColumns.TEAM_NAME],
+      abbreviation: row[BoxScoreColumns.TEAM_ABBREVIATION],
+      wonGame: row[BoxScoreColumns.WL] === 'W',
+      advancedStats: {
+        pointsScored: row[BoxScoreColumns.PTS]
+      }
+    };
+  }
+
+  private enhanceBoxScore(boxScore: BoxScore, datePlayed: string): void {
+    const advancedTeamStats = this.getAdvancedTeamStats(datePlayed);
+
+    this.loadAdvancedStats(boxScore.homeTeam, advancedTeamStats);
+    this.loadAdvancedStats(boxScore.awayTeam, advancedTeamStats);
+  }
+
+  private loadAdvancedStats(team: BoxScoreTeam, advancedTeamStats: Team[]): void {
+    const teamStats = advancedTeamStats.find(stats => stats.teamId === team.teamId);
+
+    team.advancedStats = {
+      pointsScored: team.advancedStats.pointsScored,
+      ...teamStats.advancedStats
+    };
+  }
+
+  private determineWinningCharacteristics(boxScore: BoxScore): void {
+    const winningTeam = boxScore.homeTeam.wonGame ? boxScore.homeTeam : boxScore.awayTeam;
+    const losingTeam = boxScore.homeTeam.wonGame ? boxScore.awayTeam : boxScore.homeTeam;
+
+    boxScore.winningCharacteristics = {
+      wasHomeTeam: boxScore.homeTeam.wonGame,
+      moreOffensivelyEfficient: winningTeam.advancedStats.offensiveEfficiency > losingTeam.advancedStats.offensiveEfficiency,
+      offensiveEfficiencyGap: this.formattingService.roundToNthDigit(winningTeam.advancedStats.offensiveEfficiency - losingTeam.advancedStats.offensiveEfficiency, 3),
+      moreDefensivelyEfficient: winningTeam.advancedStats.defensiveEfficiency < losingTeam.advancedStats.defensiveEfficiency,
+      defensiveEfficiencyGap: this.formattingService.roundToNthDigit(losingTeam.advancedStats.defensiveEfficiency - winningTeam.advancedStats.defensiveEfficiency, 3),
+      hadHigherWinningPercentage: winningTeam.advancedStats.winningPercentage > losingTeam.advancedStats.winningPercentage,
+      winningPercentageGap: this.formattingService.roundToNthDigit(winningTeam.advancedStats.winningPercentage - losingTeam.advancedStats.winningPercentage, 3),
+      pointGap: winningTeam.advancedStats.pointsScored - losingTeam.advancedStats.pointsScored
+    };
+  }
+
+  private getLatestFileDate(prefix: string): string {
+    const files = fs.readdirSync(Constants.dataDirectory).filter(file => file.startsWith(prefix)).sort();
+    return files[files.length - 1].replace(`${prefix}-`, '').replace('.json', '').replace(/-/g, '');
   }
 }
