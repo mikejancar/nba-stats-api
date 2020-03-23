6d9cad0733283f976a5d5c7e84b9679a56eb43d7
Michael Jancar <mikejancar@outlook.com>
2020-03-23 12:42:52 -0400

    Added ability to serve team logos

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { join } from 'path';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/public/' });
   app.enableCors();
   await app.listen(3000);
 }
